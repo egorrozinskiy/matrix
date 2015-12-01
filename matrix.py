c0d66c5137f3905f2c13f46ba79fdc3f0025abf7
egorrozinskiy <egor.rozinskiy@mail.ru>
2015-12-01 12:04:59 +0300

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -10,3 +10,21 @@ class Matrix:
         self.A = [[0]*n for i in range(m)]
         self.m = m
         self.n = n
+    def __add__(self, other):
+        if self.m == other.m and self.n == other.n:
+            for j in range(self.m):
+                for i in range(self.n):
+                    self.A[j][i] += other.A[j][i]
+            return self
+        else:
+            print('Размеры матриц не совпадают')
+    def __eq__(self, other):
+        if self.m!=other.m or self.n!=other.n:
+            raise Exception(ValueError)
+        else:
+            for i in range(self.n):
+                for j in range(self.m):
+                    if self.value[i][j]!=other.value[i][j]:
+                        return False
+            return True
+    
